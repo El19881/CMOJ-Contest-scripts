198912b270fa5685960cbb080b0de3f4bf16f910
Ariel Ferensztajn <77733595+El19881@users.noreply.github.com>
2023-06-23 19:29:26 +0200

    Create ccc07j1 (Who is in the Middle?).py

---
+++ ccc07j1 (Who is in the Middle?).py
@@ -0,0 +1,13 @@
+list_num = []
+
+
+number1 = int(input())
+list_num.append(number1)
+number2 = int(input())
+list_num.append(number2)
+number3 = int(input())
+list_num.append(number3)
+
+sorted_list=sorted(list_num)
+
+print(sorted_list[1])
