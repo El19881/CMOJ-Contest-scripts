180257d9642d72217849baa51fc71e9230e2a79e
Ariel Ferensztajn <77733595+El19881@users.noreply.github.com>
2023-06-23 19:28:33 +0200

    Create dmopc16c1p0 (C.C. and Cheese-kun).py

---
+++ dmopc16c1p0 (C.C. and Cheese-kun).py
@@ -0,0 +1,9 @@
+width = int(input())
+extra = int(input())
+
+if width == 3 and extra >=95:
+    print("C.C. is absolutely satisfied with her pizza.")
+elif width == 1 and extra <= 50:
+    print("C.C. is fairly satisfied with her pizza.")
+else:
+    print("C.C. is very satisfied with her pizza.")
