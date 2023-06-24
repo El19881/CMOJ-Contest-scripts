367a76108a6238ebc45da6118b92ddadac945067
Ariel Ferensztajn <77733595+El19881@users.noreply.github.com>
2023-06-24 20:03:25 +0200

    Create coci16c1p1 (Tarifa).py

---
+++ coci16c1p1 (Tarifa).py
@@ -0,0 +1,11 @@
+monthly_mb = int(input())
+n = int(input())
+
+excess = 0
+
+for i in range(n):
+    used = int(input())
+    
+    excess = excess + monthly_mb - used
+
+print(excess + monthly_mb)
